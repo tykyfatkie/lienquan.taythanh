e423a97af7d8f439f0df8d58d7a272b4efc3834e
tykyphatky <gg.fctaiphat@yahoo.com>
2024-09-25 00:58:28 +0700

    fsdgdsg

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/liqui.taythanh/",
   plugins: [react()],
   resolve: {
     alias: {
